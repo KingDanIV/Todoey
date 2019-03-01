53e1a2d6f7fe08585130011491a4fd8bb9ad50b6
Daniel Earl <danearl4@gmail.com>
2019-03-01 13:42:28 +0000

    Convert to MVC design pattern.

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,20 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Daniel Earl on 28/02/2019.
+//  Copyright © 2019 Daniel Earl. All rights reserved.
+//
+//  This is the item class that stores a task title and whether it has been comepleted
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
+
+
+
